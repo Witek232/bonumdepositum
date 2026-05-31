0255cefc5e4fe77abccc27bd120f5bdc73f3fb42
Witek232 <witek232@yandex.com>
2026-05-31 13:37:00 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 // 👇 Nowe importy dla konfiguracji Markdown
@@ -9,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://bonumdepositum.eu',
-  integrations: [tailwind(), sitemap(), mdx()],
+  integrations: sitemap(), mdx()],
   i18n: {
     defaultLocale: 'pl',
     locales: ['pl', 'en', 'es'],
