2a33d399ce1cff26dc07b0f1e20c271fa6d2b546
Witek232 <witek232@yandex.com>
2026-05-31 16:19:07 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
+import alpinejs from '@astrojs/alpinejs';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import remarkToc from 'remark-toc';
 import tailwindcss from '@tailwindcss/vite';
+import { satteri } from '@astrojs/markdown-satteri';
 
 export default defineConfig({
   site: 'https://bonumdepositum.eu',
-  integrations: [sitemap(), mdx()],  // ✅ Poprawione: dodany nawias [ i przecinek
+  integrations: [sitemap(), mdx()], alpinejs(), // ✅ Poprawione: dodany nawias [ i przecinek
   i18n: {
     defaultLocale: 'pl',
     locales: ['pl', 'en', 'es'],
